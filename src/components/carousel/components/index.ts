bbef5a1b59063326dfd5cd209877dec71620d28f
peng.yan <peng.yan@ticketingbox.com>
2023-01-07 18:01:07 -0500

    carousel

---
+++ src/components/carousel/components/index.ts
@@ -0,0 +1,67 @@
+import { useRef, useEffect, useState, useCallback } from 'react'
+import EmblaCarousel, {
+  EmblaCarouselType,
+  EmblaOptionsType,
+  EmblaPluginType,
+} from 'embla-carousel'
+import { arePluginsEqual, canUseDOM } from './utils'
+
+type EmblaViewportRefType = <ViewportElement extends HTMLElement>(
+  instance: ViewportElement | null,
+) => void
+
+export type UseEmblaCarouselType = [
+  EmblaViewportRefType,
+  EmblaCarouselType | undefined,
+]
+
+function useEmblaCarousel(
+  options: EmblaOptionsType = {},
+  plugins: EmblaPluginType[] = [],
+): UseEmblaCarouselType {
+  const optionsHandler = useRef(EmblaCarousel.optionsHandler())
+  const storedOptions = useRef(options)
+  const storedPlugins = useRef(plugins)
+  const [embla, setEmbla] = useState<EmblaCarouselType>()
+  const [viewport, setViewport] = useState<HTMLElement>()
+
+  const reInit = useCallback(() => {
+    if (embla) embla.reInit(storedOptions.current, storedPlugins.current)
+  }, [embla])
+
+  // eslint-disable-next-line consistent-return
+  useEffect(() => {
+    if (canUseDOM() && viewport) {
+      EmblaCarousel.globalOptions = useEmblaCarousel.globalOptions
+      const newEmbla = EmblaCarousel(
+        viewport,
+        storedOptions.current,
+        storedPlugins.current,
+      )
+      setEmbla(newEmbla)
+      return () => newEmbla.destroy()
+    } else {
+      setEmbla(undefined)
+    }
+  }, [viewport, setEmbla])
+
+  useEffect(() => {
+    if (optionsHandler.current.areEqual(storedOptions.current, options)) return
+
+    storedOptions.current = options
+    reInit()
+  }, [options, reInit])
+
+  useEffect(() => {
+    if (arePluginsEqual(storedPlugins.current, plugins)) return
+
+    storedPlugins.current = plugins
+    reInit()
+  }, [plugins, reInit])
+
+  return [<EmblaViewportRefType>setViewport, embla]
+}
+
+useEmblaCarousel.globalOptions = <EmblaOptionsType | undefined>undefined
+
+export default useEmblaCarousel
