1488464f7afefa67cacb8132f4f95512168d53d7
peng.yan <peng.yan@ticketingbox.com>
2023-02-17 14:58:15 -0500

    im

--- src/components/featured/featured.tsx
+++ src/components/featured/featured.tsx
@@ -4,6 +4,8 @@ import {Link} from 'react-router-dom'
 import {MainProps} from '../../types'
 import HeroCard from '../templates/HeroCard'
 import {sectionMap} from '../../config'
+import {useAppSelector} from '../../app/hooks'
+import {RootState} from '../../app/store'
 
 export type FeaturedProps = {
   data: MainProps
@@ -11,34 +13,27 @@ export type FeaturedProps = {
 
 export default function Featured({data}: FeaturedProps) {
   const {
-    category: {
-      itemsFeatured,
-      itemsShenyunIntroduction,
-      itemsShenyunTrailers,
-      itemsReviews,
-      itemsEditorsPick,
-      itemsSyso,
-      itemsStartsOfShenyun,
-      itemsPersecution,
-      itemsMorefromArtists,
-    },
-  } = data
+    content: {translation, langCode},
+  } = useAppSelector((state: RootState) => state.intro)
+
   return (
     <div className="featured innerPaddingAlignHeader">
-      {Object.entries(sectionMap).filter(item=>item[0]!=='featured').map(section => {
-        const [sectionUrl, sectionContent] = section
-        return (
-          <div key={sectionUrl}>
-            <div className="mt-0 mb-0 text-center">
-              <h2 className="text-2xl md:text-4xl text-[#524941] OpenSans__font">{sectionContent.title}</h2>
-            </div>
+      {Object.entries(sectionMap)
+        .filter(item => item[0] !== 'featured')
+        .map(section => {
+          const [sectionUrl, sectionContent] = section
+          return (
+            <div key={sectionUrl}>
+              <div className="mt-0 mb-0 text-center">
+                <h2 className="text-2xl md:text-4xl text-[#524941] OpenSans__font">{translation[sectionContent.title]}</h2>
+              </div>
 
-            {sectionContent.content.map((item: React.Key | null | undefined) => (
-              <HeroCard key={item} keyName={item} sectionName={sectionUrl} />
-            ))}
-          </div>
-        )
-      })}
+              {sectionContent.content.map((item: React.Key | null | undefined) => (
+                <HeroCard key={item} keyName={item} sectionName={sectionUrl} />
+              ))}
+            </div>
+          )
+        })}
     </div>
   )
 }
